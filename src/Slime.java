4cb91211c8ccf02961dd15d465d3059aa311b7ba
Alexander Moon <ajmoon2@wisc.edu>
2018-11-29 15:37:03 -0600

    Updated src structure + Updated Slime.java
    Imported the Git structure into my computer and fleshed out the basic characteristics and actions of a slime.

--- src/Slime.java
+++ src/Slime.java
@@ -10,23 +10,59 @@ public class Slime {
   private int xVel; //X-Velocity of Slime (Right is positive)
   private int yVel; //Y-Velocity of Slime (Up is positive)
   
+  private final int MIN_X; //The x-value of the left boundary
+  private final int MAX_X; //The x-value of the right boundary
+  private final int MAX_Y; //The floor
+  private final int WIDTH; //The width of the slime
+  private final int HEIGHT; //The height of the slime
+  
+  private final int G; //The acceleration of gravity
+  
   private final char UP_KEY; //Character that represents keybinding of jump
   private final char LEFT_KEY; //Character that represents keybinding of moving left
   private final char RIGHT_KEY; //Character that represents keybinding of moving right
   
   private Ball ball; //Private instance of the ball for easy access
   
-  public Slime(int x, int y, char up, char left, char right, Ball ball) {
+  public Slime(int x, int y, int minX, int maxX, int maxY, char up, int width, int height,
+      char left, char right, Ball ball) {
     xPos = x;
     yPos = y;
     xVel = 0;
     yVel = 0;
     
+    WIDTH = width;
+    HEIGHT = height;
+    //Corrects boundaries for width and height of slime
+    MIN_X = minX + (WIDTH / 2);
+    MAX_X = maxX - (WIDTH / 2);
+    MAX_Y = maxY - (HEIGHT / 2);
+    
     UP_KEY = up;
     LEFT_KEY = left;
     RIGHT_KEY = right;
     
+    G = 1;
+    
     this.ball = ball;
+  }
+  
+  /**
+   * Moves the slime based on current x and y velocity
+   */
+  public void move() {
+    //If statement prevents slime from leaving boundaries
+    if (!(xPos == MAX_X && xVel > 0) && !(xPos == MIN_X && xVel < 0)) {
+      xPos += xVel;
+    }
+    //Moves slime up and down, then resolves gravity and floor
+    yPos += yVel;
+    if (yPos < MAX_Y) {
+      yVel += G;
+    } else {
+      yPos = MAX_Y;
+      yVel = 0;
+    }
   }
 
   /**
@@ -92,9 +128,6 @@ public class Slime {
   public Ball getBall() {
     return ball;
   }
-  
-  
-  
 }
 
 /**
